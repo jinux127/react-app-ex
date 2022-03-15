7d2461933c2beaf9bfe01d15f4c86b67a0b39a83
jinwoo <wwo1278@gmail.com>
2022-03-15 19:58:11 +0900

    배포 시 화면 출력 수정

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Home from './routes/Home';
 
 const App = () => {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
-                <Route path="${process.env.PUBLIC_URL}/movie/:id" element={<Detail />}/>
-                <Route path="${process.env.PUBLIC_URL}/" element={<Home />}/>
+                <Route path="/movie/:id" element={<Detail />}/>
+                <Route path="/" element={<Home />}/>
             </Routes>
         </Router>);
 }
